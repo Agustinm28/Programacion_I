be85aaab29576ac1c5d632806635c49d958acd32
Agustinm28 <agustinm28@gmail.com>
2022-03-18 12:24:56 -0300

    Backend Update

--- backend/app.py
+++ backend/app.py
@@ -1,8 +1,7 @@
-from flask import Flask
 import os
-from dotenv import load_dotenv
-load_dotenv()
-app = Flask(__name__)
+from main import create_app
+app = create_app()
+app.app_context().push()
 if __name__ == '__main__':
     app.run(debug=True,port=os.getenv("PORT"))
 #comentario
