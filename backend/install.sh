c8d9f406efa13c78b468a3e8e483993389596176
Agustinm28 <agustinm28@gmail.com>
2022-03-29 10:17:04 -0300

    Comments

--- backend/install.sh
+++ backend/install.sh
@@ -1,6 +1,6 @@
-command -v python3 >/dev/null 2>&1 || { echo >&2 "python3 is not installed.  Aborting."; exit 1; }
-dpkg-query -l python3-venv > /dev/null 2>&1 || { echo >&2 "python3-venv is not installed. Aborting.";exit 1;}
-python3 -m venv .
-source bin/activate
-pip install --upgrade pip
-pip3 install -r requirements.txt
+command -v python3 >/dev/null 2>&1 || { echo >&2 "python3 is not installed.  Aborting."; exit 1; } # Verificar si el comando existe (valido ubuntu)
+dpkg-query -l python3-venv > /dev/null 2>&1 || { echo >&2 "python3-venv is not installed. Aborting.";exit 1;} # Verificar si un paquete esta instalado (valido Ubuntu)
+python3 -m venv . # Crear entorno virtual en carpeta actual
+source bin/activate # Activar entorno virtual
+pip install --upgrade pip # Actualiza pip
+pip3 install -r requirements.txt # Instala la lista de librerias especificadas en el archivo requeriments
