4bfedf470929d4680649ed6d8ddab04ee3d84f7c
Bruno-Orbelli <b.orbelli@alumno.um.edu.ar>
2022-04-01 13:23:13 -0300

    Init correcto

--- backend/main/models/__init__.py
+++ backend/main/models/__init__.py
@@ -1,7 +1,3 @@
 from .Poems import Poem as PoemModel
 from .Poets import Poet as PoetModel
-<<<<<<< HEAD
-# from .Ratings import Rating as RatingModel
-=======
 from .Ratings import Rating as RatingModel
->>>>>>> b8cd034bd6adfdcb851513eb5fc8dfc846b97ca7
