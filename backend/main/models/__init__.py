aca55fa91683e2b4ea91219478af6abf219ee369
Bruno-Orbelli <b.orbelli@alumno.um.edu.ar>
2022-04-01 11:50:33 -0300

    Carpeta models

---
+++ backend/main/models/__init__.py
@@ -0,0 +1,6 @@
+from .Poems import Poem as PoemModel
+from .Poems import Poems as PoemsModel
+from .Poets import Poets as PoetsModel
+from .Poets import Poet as PoetModel
+from .Ratings import Rating as RatingModel
+from .Ratings import Ratings as RatingsModel
