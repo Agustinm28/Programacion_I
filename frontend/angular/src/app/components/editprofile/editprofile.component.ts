e808174153a62c782318f6687a8e2cdc33deb775
Agustín Montaña <81200417+Agustinm28@users.noreply.github.com>
2022-12-05 09:32:39 -0300

    working pages

--- frontend/angular/src/app/components/editprofile/editprofile.component.ts
+++ frontend/angular/src/app/components/editprofile/editprofile.component.ts
@@ -1,4 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { PoetService } from 'src/app/services/poet.service';
+import { AuthService } from 'src/app/services/auth.service';
 
 @Component({
   selector: 'app-editprofile',
@@ -6,10 +10,32 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./editprofile.component.css']
 })
 export class EditprofileComponent implements OnInit {
+  
+  userForm!: FormGroup;
+  token: any = localStorage.getItem("token")
+  @Input("loggedPoet") poet: any
 
-  constructor() { }
+  constructor(
+    private poetService: PoetService,
+    private formBuilder: FormBuilder,
+    private router: Router,
+    private authService: AuthService
+  ) { }
 
   ngOnInit(): void {
+    this.userForm = this.formBuilder.group({
+      name: [this.poet.name, Validators.required], 
+      lname: [this.poet.lname, Validators.required],
+      uname: [this.poet.uname, Validators.required],
+      mail: [this.poet.mail, Validators.required],
+      passw: [this.poet.passw, Validators.required]
+    });
+    
+  }
+
+  change_data () {
+    this.poetService.putPoet(this.token, this.poet.id, this.userForm);
+    this.router.navigate(["/login/admin/profile"])
   }
 
 }
--- frontend/angular/src/app/pages/editprofilepage/editprofilepage.component.ts
+++ frontend/angular/src/app/pages/editprofilepage/editprofilepage.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from 'src/app/services/auth.service';
-import { PoemService } from 'src/app/services/poem.service';
 import { PoetService } from 'src/app/services/poet.service';
 
 @Component({
@@ -10,19 +9,16 @@ import { PoetService } from 'src/app/services/poet.service';
 })
 export class EditprofilepageComponent implements OnInit {
 
-  ownPoems: any = []
   token: any = localStorage.getItem("token")
   loggedPoet: any
 
   constructor(
-    private poemService: PoemService,
     private poetService: PoetService
   ) { }
 
   ngOnInit(): void {
     let id = JSON.parse(window.atob(this.token.split('.')[1])).id;
     this.poetService.getPoet(id, this.token).subscribe((data: any) => this.loggedPoet = data)
-    this.poemService.getPoems(this.token, {"poet_id": id}).subscribe((data: any) => this.ownPoems = data.poem)
   }
 
 }
